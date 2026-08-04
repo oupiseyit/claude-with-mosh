6f688b8806154bcf3930436da7d49c70b988d012
pisey.sk <T4FbVNfk8CFaV*k>
2026-08-04 20:30:10 +0700

    Set up Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     return (
         <div className="p-4">
             <p className="font-bold p-4 text-3xl">{message}</p>
-            <Button>Click me</Button>
+            <Button>Click me !</Button>
         </div>
     );
 }
